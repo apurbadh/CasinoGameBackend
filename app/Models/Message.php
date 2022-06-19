c0ec575fae404e6e01661f9b763494c32471e3b0
Apurba Adhikari <erikduke182@gmail.com>
2022-06-19 13:01:36 +0545

    Created a model and migration for chat table

---
+++ app/Models/Message.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+        'user_id'
+    ];
+}
